c83876a77905f6bcba103c5e54612ede58a49d49
Omer <omer.raziuddin1@gmail.com>
2025-09-05 21:05:56 -0500

    Added redirect lambda function with GetItem and UpdateItem and IAM in tf

---
+++ infra/lambda/redirect/handler.py
@@ -0,0 +1,90 @@
+import os
+import json
+import re
+import time
+import boto3
+from botocore.exceptions import ClientError
+
+TABLE_NAME = os.environ.get("TABLE_NAME", "")
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(TABLE_NAME)
+
+CODE = re.compile(r"^[A-Z0-9]{6}$") #regex to check if the code is valid
+
+PATH_CODE = re.compile(r"/r/([A-Z0-9]{6})(?:/)?$") #in case it is something liek "/r/83A5E8" extract the code only
+
+
+#api gateway expects a dict with statuscode, headers and body
+def json_response(status, data): #func to return json with cors
+    return { 
+        "statusCode": status, #the numeric http status
+        "headers": {
+            "Content-Type": "application/json", #its json
+            "Access-Control-Allow-Origin": "*", #allow calls from any origin 
+            # "Access-Control-Allow-Credentials": True
+        },
+        "body": json.dumps(data), #the api gateway string body
+    }
+    
+    
+#support direct lambda invoke and api gateway http api 
+def read_from_event(event): #funvction to read short code from event
+    
+    if isinstance(event, dict): #only handle dict events
+        
+        #{"code": "..."}
+        if "code" in event: #if they key "code" exists 
+            return str(event["code"]) #then return as string
+        
+        #{"short_code": "..."}
+        if "short_code" in event:  #now if diff invoke "short_code"
+            return str(event["short_code"])
+        
+        #now if http api route parameters
+        path_parameters = event.get("pathParameters") or event.get("path_parameters") or {}
+        if isinstance(path_parameters, dict) and "code" in path_parameters:
+            return str(path_parameters["code"]) #return the path params
+        
+        #now last one, parse the raw string
+        path = event.get("rawPath") or event.get("path") or "" #diff keys across gateways
+        check = PATH_CODE.search(path) #try to match the code 
+        if check:  #if matched then return the ccode
+            return check.group(1)
+        
+    return ""
+
+
+#now the main lambda handler which will look up the short code and issue an http redirect
+def lambda_handler(event, context): 
+    code = read_from_event(event) #get the code from the event
+    
+    if not code: 
+        return json_response(400, {"error": "Invalid code."}) #if no code, return 400
+    
+    try: 
+        dyTable = table.get_item(Key={"short_code": code}) #try to get the item from dynamodb
+        item = dyTable.get("Item") #get the item from the response
+        
+    except ClientError as e: 
+        return json_response(500, {"error" : f"DynamoDB internal error: {e.response['Error']['Message']}"})
+    
+    if not item: 
+        return json_response(404, {"error": "Short code not found."}) #if no item, return 404
+    
+    long_url = item["long_url"]  #the dest url 
+    
+    try:
+        table.update_item(
+            Key = {"short_code": code}, #the key to update, same key
+            UpdateExpression = "ADD clicks :one SET last_accessed = :ts", #the update expression
+            ExpressionAttributeValues = {":one": 1,
+                                         ":ts": int(time.time())} #the value to increment by
+        )
+    except ClientError as e:
+        pass
+    
+    return{  #return a 301 redirect which the api gateway turns into an http response with location header
+           "statusCode" : 301,  #301 means moved permanently
+           "headers": {"Location": long_url}, #the browser will follow this url
+            "body" : "" #body can be empty 
+    }   
--- infra/main.tf
+++ infra/main.tf
@@ -116,4 +116,110 @@ resource "aws_iam_role_policy" "shortener_dynamoDB" {
     }]
   })
 }
-  
+
+
+
+
+#Redirect pieces
+
+#package the redirect lambda into a zip
+data "archive_file" "redirect_zip" {
+  type        = "zip"                               #create a .zip archive 
+  source_dir  = "${path.module}/lambda/redirect"    #source is the folder containing the redirect
+  output_path = "${path.module}/build/redirect.zip" #and write it in the build so it gets gitignored
+}
+
+
+#iam roles for redirect
+resource "aws_iam_role" "redirect_role" {
+  name = "urlshortner-redirect-role" #the visible iam role name
+  #allow lambda to assume this role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+
+}
+
+
+#now get to logging permissions- cloudWatch logs
+resource "aws_iam_role_policy" "redirect_basic" {
+  name = "urlshortner-redirect-basic"
+  role = aws_iam_role.redirect_role.id #attaching the role to the policy, already created earlier
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid    = "AllowCreateWriteLogs",
+      Effect = "Allow",
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+#now add dynamoDB permissions to read and update table
+resource "aws_iam_role_policy" "redirect_dynamodb" {
+  name = "urlshortner-redirect-dynamodb"
+  role = aws_iam_role.redirect_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid    = "DynamoTableReadUpdate",
+      Effect = "Allow",
+      Action = [
+        "dynamodb:GetItem",   #read the mapping
+        "dynamodb:UpdateItem" #update by incremeenting clicks and set last accesssed time
+      ],
+      Resource = aws_dynamodb_table.url.arn #least priv- this table only
+    }]
+
+  })
+
+}
+
+
+#now create the redirect lambda function
+resource "aws_lambda_function" "redirect" {
+  function_name = "urlshortner-redirect"         #the function name in aws
+  role          = aws_iam_role.redirect_role.arn #execution role
+
+  runtime = "python3.11"             #same runtime as the shortener lambda function
+  handler = "handler.lambda_handler" #same handler as the shortener lambda function, the file.fuiinction inside the .zip
+
+  filename         = data.archive_file.redirect_zip.output_path         #the zip file to upload to lambda
+  source_code_hash = data.archive_file.redirect_zip.output_base64sha256 #the hash of the zip file to ensure the lambda func is updated when the zip changes
+
+  timeout       = 5 #5 second timeout
+  memory_size   = 256
+  architectures = ["arm64"] #arm64
+
+  environment {
+    variables = {
+      TABLE_NAME = var.table_name #the table name to store the short code and long url
+
+    }
+  }
+
+  tags = {
+    Project = "serverless-url-shortner"
+    Env     = "dev"
+  }
+
+
+}
+
+
+
